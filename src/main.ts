5d8b00aa376b497cf55dd3ef97c1e4e3e91f3f5d
zsevic <zsevic95@gmail.com>
2026-04-25 22:09:09 +0200

    Connect with the front-end

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.FRONTEND_ORIGIN ?? "http://localhost:3001",
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
